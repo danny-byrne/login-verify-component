22776791fefa09ea716c1ad95b5428a8c469ff12
Danny Byrne <dannybyrne@DANIELs-MacBook-Pro.local>
2020-06-04 20:02:40 -0700

    basic test boilerplate added

--- src/actions/__tests__/actions.test.js
+++ src/actions/__tests__/actions.test.js
@@ -1,10 +1,6 @@
 import * as actions from '../actions';
 import { UPDATE_USER, RESET_USER, SUBMIT } from '../actionTypes';
-import fetchMock from 'fetch-mock';
-import configureMockStore from 'redux-mock-store';
-import thunk from 'redux-thunk';
-const middlewares = [thunk];
-const mockStore = configureMockStore(middlewares);
+
 
 describe('actions', () => {
   it("should create an action to add a user", () => {
@@ -25,43 +21,3 @@ describe('actions', () => {
   })
 })
 
-describe('async actions', () => {
-  afterEach(() => {
-    fetchMock.restoter()
-  })
-
-  xit('creates an async post request in submit', () => {
-    fetchMock.getOnce('http://localhost:5555/createuser', {
-      body: {
-        firstName: 'Joe',
-        lastName: 'schmo',
-        userName: 'JS',
-        password: 'pass',
-        confirmPassword: 'pass'
-      },
-      headers: { 'content-type': 'application/json' }
-    })
-
-    const expectedActions = [
-      { type: SUBMIT,
-        body: {
-          firstName: 'Joe',
-          lastName: 'schmo',
-          userName: 'JS',
-          password: 'pass',
-          confirmPassword: 'pass'
-        }
-      }
-    ]
-    const store = mockStore({ firstName: "",
-    lastName: "",
-    userName: "",
-    email: "",
-    password: "",
-    confirmPassword: "" })
-
-    return store.dispatch(actions.SUBMIT()).then(() => {
-      expect(store.getActions()).toEqual(expectedActions)
-    })
-  })
-})
