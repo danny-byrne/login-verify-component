f1673951aa77efdbdb8d1b02b06efdd61fe48af1
Danny Byrne <dannybyrne@DANIELs-MacBook-Pro.local>
2020-06-04 19:34:11 -0700

    refactoring async tests

--- src/actions/__tests__/actions.test.js
+++ src/actions/__tests__/actions.test.js
@@ -1,5 +1,10 @@
 import * as actions from '../actions';
 import { UPDATE_USER, RESET_USER, SUBMIT } from '../actionTypes';
+import fetchMock from 'fetch-mock';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
 
 describe('actions', () => {
   it("should create an action to add a user", () => {
@@ -10,13 +15,53 @@ describe('actions', () => {
     }
     expect(actions.updateUser(user)).toEqual(expected)
   })
-  // it("should create an action to reset the user information",  () => {
-  //   const event = 'event';
-  //   const expected = {
-  //     type: RESET_USER,
-  //     payload: event
-  //   }
-  //   expect(actions.resetUser(event)).toEqual(expected)
-  // })
+  it("should create an action to reset the user information",  () => {
+    const event = 'event';
+    const expected = {
+      type: RESET_USER,
+      payload: event  
+    }
+    expect(actions.resetUser(event)).toEqual(expected)
+  })
+})
 
+describe('async actions', () => {
+  afterEach(() => {
+    fetchMock.restoter()
+  })
+
+  xit('creates an async post request in submit', () => {
+    fetchMock.getOnce('http://localhost:5555/createuser', {
+      body: {
+        firstName: 'Joe',
+        lastName: 'schmo',
+        userName: 'JS',
+        password: 'pass',
+        confirmPassword: 'pass'
+      },
+      headers: { 'content-type': 'application/json' }
+    })
+
+    const expectedActions = [
+      { type: SUBMIT,
+        body: {
+          firstName: 'Joe',
+          lastName: 'schmo',
+          userName: 'JS',
+          password: 'pass',
+          confirmPassword: 'pass'
+        }
+      }
+    ]
+    const store = mockStore({ firstName: "",
+    lastName: "",
+    userName: "",
+    email: "",
+    password: "",
+    confirmPassword: "" })
+
+    return store.dispatch(actions.SUBMIT()).then(() => {
+      expect(store.getActions()).toEqual(expectedActions)
+    })
+  })
 })
