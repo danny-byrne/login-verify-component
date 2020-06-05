9ccd0e10d2a48ecf6815c908e48c6b7b8e1b6a81
Danny Byrne <dannybyrne@DANIELs-MacBook-Pro.local>
2020-06-04 18:49:59 -0700

    starting test runs

---
+++ src/actions/__tests__/actions.test.js
@@ -0,0 +1,22 @@
+import * as actions from '../actions';
+import { UPDATE_USER, RESET_USER, SUBMIT } from '../actionTypes';
+
+describe('actions', () => {
+  it("should create an action to add a user", () => {
+    const user = 'user';
+    const expected = {
+      type: UPDATE_USER,
+      payload: user
+    }
+    expect(actions.updateUser(user)).toEqual(expected)
+  })
+  // it("should create an action to reset the user information",  () => {
+  //   const event = 'event';
+  //   const expected = {
+  //     type: RESET_USER,
+  //     payload: event
+  //   }
+  //   expect(actions.resetUser(event)).toEqual(expected)
+  // })
+
+})
